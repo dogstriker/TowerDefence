bf084eaf0f78894a1a25f0f767c4190fb9f80c7b
dogstriker <48491633+dogstriker@users.noreply.github.com>
2019-08-08 21:02:39 +0300

    8.08

---
+++ TD_2/Behaviors/EnemyLightTankAI.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GameMaps;
+namespace TowerDefence
+{
+    class EnemyLightTankAI:Behavior
+    {
+        
+        double SquareRadius;
+        List<UGameObjectBase> targetList;
+        public EnemyLightTankAI(List<UGameObjectBase> l)
+        {
+            targetList = l;
+            SquareRadius = unit.Par.Range * unit.Par.Range;
+        }
+        public override void Act()
+        {
+ 	    
+        }
+        void Init()
+        {
+            UGameObjectBase tempTarget=null;
+            double tempMinAngle = 360;
+            double a;
+            double r;
+            double MinRange = 9999999999;
+            UGameObjectBase tempOTRTarget=null;
+        //поиск цели в радиусе поражения
+            for (int i = 0; i < targetList.Count; i++)
+            {
+                r = (targetList[i].Par.X - unit.Par.X) * (targetList[i].Par.X - unit.Par.X) + (targetList[i].Par.Y - unit.Par.Y) * (targetList[i].Par.Y - unit.Par.Y);
+                if (r < SquareRadius)
+                {
+
+                    a = GameMath.CompareAngles(unit.Par.Angle, GameMath.GetAngleOfVector(targetList[i].Par.X - unit.Par.X, targetList[i].Par.Y - unit.Par.Y));
+                    if (a < tempMinAngle)
+                    {
+                        tempMinAngle = a;
+                        tempTarget = targetList[i];
+                    }
+                }
+                else 
+                {
+                    if (r < MinRange)
+                    {
+                        MinRange = r;
+                        tempOTRTarget = targetList[i];
+
+                    }
+                }
+            }
+            //поиск цели вне радиуса при необходимости
+            if (tempTarget == null)
+            { 
+            
+            }
+        }
+    }
+}
